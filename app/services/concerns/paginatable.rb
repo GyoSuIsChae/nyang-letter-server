5be9388b8fffdf90e17bd5a63ee9f1185ccf0ed2
gyogo <dfva1234@gmail.com>
2022-09-26 22:35:08 +0900

    🔨NLT-51 Paginatable 작성

---
+++ app/services/concerns/paginatable.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Paginatable
+  extend ActiveSupport::Concern
+
+  included do
+    integer :page, default: 1
+    integer :size, default: 30
+  end
+
+  def paginated
+    result.page(page).per(size)
+  end
+end
