4c35404df2a85e944a64dd365159dacc5b3cad5b
gyogo <dfva1234@gmail.com>
2023-03-02 20:09:11 +0900

    NLT-86 유저 정보 수정 서비스 로직 작성

---
+++ app/services/users/update_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Users::UpdateService < ServiceBase
+  record :user, finder: :find_by_uid!, class: User
+  string :uid
+  string :nickname, :is_complete_tutorial, default: nil
+
+  validates :is_complete_tutorial, inclusion: %w[Y N]
+
+  def execute
+    if uid != user.uid
+      raise UnauthorizedError.new
+    end
+
+    @user = User.by_uid(uid)
+
+    if nickname != nil
+      # 닉네임 중복 체크
+      if User.find_by(nickname: nickname).present?
+        raise Users::IsAlreadyExistNicknameError.new
+      end
+
+      @user.update(nickname: nickname)
+    end
+
+    if is_complete_tutorial != nil
+      @user.update(is_complete_tutorial: is_complete_tutorial)
+    end
+
+    @user.last
+  end
+end
